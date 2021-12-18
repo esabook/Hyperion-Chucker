b86b822f03b3222c5a10aadd8e10fb50aa617f87
Egit <esa_book@outlook.com>
2021-12-19 01:47:09 +0700

    Migrate Chuck to Chucker

--- hyperion-chucker/src/main/java/io/github/esabook/hyperion_chucker/ChuckPlugin.java
+++ hyperion-chucker/src/main/java/io/github/esabook/hyperion_chucker/ChuckPlugin.java
@@ -1,8 +1,10 @@
-package com.commit451.hyperion.chuck;
+package io.github.esabook.hyperion_chucker;
 
 import com.google.auto.service.AutoService;
 import com.willowtreeapps.hyperion.plugin.v1.Plugin;
 import com.willowtreeapps.hyperion.plugin.v1.PluginModule;
+
+import io.github.esabook.hyperion_chucker.ChuckModule;
 
 @SuppressWarnings("unused")
 @AutoService(Plugin.class)
